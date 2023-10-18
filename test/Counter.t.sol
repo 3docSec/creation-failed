b9106795eaf91407c0a900e250d4684ab6756753
3doc <3docsec@users.noreply.github.com>
2023-10-18 15:24:14 +0200

    Add failing test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract BugTest is Test {
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+    address public something;
+
+    function testNoBug() public {
+        new SomeContract();
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testFailBug() public {
+        vm.pauseGasMetering();
+        new SomeContract();
     }
+}
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+contract SomeContract {
+    constructor() {
+        BugTest(msg.sender).something();
     }
 }
